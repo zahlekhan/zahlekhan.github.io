256707737b6403a77e6a424402bbf7aad4531221
Zahle Khan <zahlekhaned@gmail.com>
2017-09-17 10:31:10 +0530

    added validation

--- js/index.js
+++ js/index.js
@@ -7,23 +7,48 @@ $(function(){
 });
 
 
-    $("#personal").validate({
-    rules: {
-        email: {
+
+    $(document).ready(function () {
+
+    $('#test').validate({ // initialize the plugin
+        rules: {
+            name: {
+                required: true
+            },
+            college: {
+                required: true
+            },
+			email: {
             required: true,
             email: true
-        },
-        password: {
+			},
+			password: {
             required: true,
             minlength: 8
-        },
-        password_confirm: {
+			},
+			password_confirm: {
             required: true,
             minlength: 8,
             equalTo: "#password"
-      }
-    },
-    messages: {
+			},
+			mobile: {
+				required:true,
+				length: 10
+			},
+			address: {
+				required:true
+			},
+			pincode: {
+				required:true
+			},
+			why: {
+				required: true
+			},
+			exp: {
+				required: true
+			},
+		},
+		messages: {
         email: "Please enter a valid email address",
         password: {
           required: "Please provide a password",
@@ -33,9 +58,27 @@ $(function(){
           required: "Please provide a password",
           minlength: "Your password must be at least 8 characters long",
           equalTo: "Please enter the same password as above"
+        },
+		mobile: {
+			
+		},
+		
+    },
+        submitHandler: function (form) { // for demo
+            alert('valid form submitted'); // for demo
+            return false; // for demo
         }
-    }
-  });
+    });
+
+    $('#btn1').click(function() {
+        if ($('#test').valid()) {
+            alert('form is valid - not submitted');
+        } else {
+            alert('form is not valid');
+        }
+    });
+    
+});
 	
 
          
